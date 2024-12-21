4ae42e93e591f444654917287a4c2a8bfa8be05f
Udit Ray <132336609+uditray02@users.noreply.github.com>
2024-12-21 10:38:27 +0530

    open_rtsp.py

--- open_rtsp.py
+++ open_rtsp.py
@@ -1,7 +1,7 @@
 import cv2
 
 cv2.namedWindow("RTSP View", cv2.WINDOW_NORMAL)
-cap = cv2.VideoCapture("rtsp://192.168.1.12:8554/video_stream")
+cap = cv2.VideoCapture("rtsp://192.168.2.95:8554/video_stream")
 while True:
     
     ret, frame = cap.read()
